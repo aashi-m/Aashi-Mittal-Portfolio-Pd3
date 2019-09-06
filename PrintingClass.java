605412d2a25f4a54466969c056e56c1c40ab26d1
aashi <aashimittal19@gmail.com>
2019-09-06 00:04:42 -0700

    This is a new file I added (just simple printing) to test my github repository.

---
+++ PrintingClass.java
@@ -0,0 +1,8 @@
+public class PrintingClass
+{
+    public static void main ()
+    {
+        System.out.println("hello my name is aashi");
+        System.out.println("this is a github test");
+    }
+}
