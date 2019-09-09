cc4ff2f0f458720938cca769f5ea874798a4cbf4
aashi <aashimittal19@gmail.com>
2019-09-09 12:02:33 -0700

    Deleted, start again

--- PrintingClass.java
+++
@@ -1,8 +0,0 @@
-public class PrintingClass
-{
-    public static void main ()
-    {
-        System.out.println("hello my name is aashi");
-        System.out.println("this is a github test");
-    }
-}
