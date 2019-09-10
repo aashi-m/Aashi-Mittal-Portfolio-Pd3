9603d5ced9bb2d2b3c46da38ee2c76cd1712e75a
aashi <aashimittal19@gmail.com>
2019-09-09 23:48:46 -0700

    This is a practice if-else code, very simple, just with integers

---
+++ IfElseCode.java
@@ -0,0 +1,19 @@
+public class IfElseCode
+{
+    public static void main (String [] args)
+    {
+        int i = 532; //this is a spot where I want to put user input 
+        //or, the other idea was "if you are a girl type 'g' and then it will 
+        //say "you are a girl", and same if 'b' for boy 
+        if (i<50)
+        System.out.println ("i is less than 50");
+        else 
+        System.out.println ("i is is greater than 50");
+      /* This is the simplest if-else code there is
+       * My next goal is to insert user input, and do an if-else statment 
+       * based on that 
+       * I also need to learn how to work variables
+       */
+    }
+}
+    
